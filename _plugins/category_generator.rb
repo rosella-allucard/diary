f27ce6b6c76c4006d7e016e42c05e316fa0dea10
RosellaZ <qianzezhu@g.harvard.edu>
2025-07-02 00:06:12 -0400

    first test

---
+++ _plugins/category_generator.rb
@@ -0,0 +1,34 @@
+module Jekyll
+
+  class CategoryIndex < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category'] = category
+
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      self.data['title'] = "#{category_title_prefix}#{category}"
+    end
+  end
+
+  class CategoryGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        dir = site.config['category_dir'] || 'categories'
+        site.categories.keys.each do |category|
+          # Slugify the category name to ensure URL consistency
+          slug = Utils.slugify(category)
+          site.pages << CategoryIndex.new(site, site.source, File.join(dir, slug), category)
+        end
+      end
+    end
+  end
+
+end
